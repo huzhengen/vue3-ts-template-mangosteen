11fb743666eb1cefb99c2a42067ebdafaa35528d
hukai <huzhengen@163.com>
2022-05-21 15:22:05 +0800

    添加 vue-router

---
+++ src/About.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>about</div>
+  <div><router-link to="/">home</router-link></div>
+</template>
+
+<style></style>
---
+++ src/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>home</div>
+  <div><router-link to="/about">about</router-link></div>
+</template>
+
+<style></style>
